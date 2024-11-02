0e381a9cbaf7fd5b45cd17578bbb3e8f62183c2b
Ganesh Soni <ns1915@srmist.edu.in>
2024-11-02 23:54:52 +0530

    Added song to javascript

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
  console.log("Welcome to Spotify");
  console.log("You Taste in Music is our Priority");
- let masterplay=document.getElementById("#progress_bar");
+
+
+ let songindex=0;
+let audio=new Audio("Jaane Kyun.mp3");
+let masterplay=document.getElementById("masterplay");
+ let progress_bar=document.getElementById("#progress_bar");
+
+
  masterplay.addEventListener('click',()=>{
-    if(audioElement.paused || audioElement.currentTime<=0){
-        audioElement.play();
+    if(audio.paused || audio.currentTime<=0){
+        audio.play();
         masterplay.classList.remove("fa-regular fa-circle-play");
-        masterplay.classList.add("fa-regular fa-circle-pause");
+        masterplay.classList.add("fa-solid fa-circle-pause");
+    }
+    else{
+        audio.pause();
+        masterplay.classList.remove("fa-solid fa-circle-pause");
+        masterplay.classList.add("fa-regular fa-circle-play");
     }
  })
+ 
