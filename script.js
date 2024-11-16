fcbb3ce41258218805494d072c1972c30e47db75
Nancy Soni <ns1915@srmist.edu.in>
2024-11-16 23:53:11 +0530

    Updated script

--- script.js
+++ script.js
@@ -11,30 +11,30 @@ let mplay=document.getElementById("mplay");
 // let audio=new Audio["Jaane Kyun.mp3","Dil_Tu_Jaan_Tu.mp3","Excuses.mp3"]
 
 let song=[
-    {songname:"Bahara", filepath:"Documents/GitDemo/Spotify/Bahara1.mp3",coverpath:"Documents/GitDemo/Spotify/song1.jpeg"},
-    {songname:"Dil Tu Jaan Tu", filepath:"Documents/GitDemo/Spotify/Dil_Tu_Jaan_Tu.mp3",coverpath:"Documents/GitDemo/Spotify/song2.jpeg"},
-    {songname:"Excuses", filepath:"Documents/GitDemo/Spotify/Excuses.mp3",coverpath:"Documents/GitDemo/Spotify/song3.jpeg"},
-    {songname:"Heroine", filepath:"Documents/GitDemo/Spotify/Heroine4.mp3",coverpath:"Documents/GitDemo/Spotify/song4.jpeg"},
-    {songname:"Jaane Kyun", filepath:"Documents/GitDemo/Spotify/Jaane Kyun.mp3",coverpath:"Documents/GitDemo/Spotify/song5.jpeg"},
-    {songname:"Tu Mileya", filepath:"Documents/GitDemo/Spotify/Tu_Mileya.mp3",coverpath:"Documents/GitDemo/Spotify/song6.jpeg"},
-    {songname:"Raatan Lambiyan", filepath:"Documents/GitDemo/Spotify/Raatan_Lambiyan.mp3",coverpath:"Documents/GitDemo/Spotify/song7.jpeg"},
-    {songname:"Tum Jo Aaye", filepath:"Documents/GitDemo/Spotify/Tum_Jo_Aaye.mp3",coverpath:"Documents/GitDemo/Spotify/song8.jpeg"},
-    {songname:"Tum Se", filepath:"Documents/GitDemo/Spotify/Tum_Se.mp3",coverpath:"Documents/GitDemo/Spotify/song9.jpeg"},
-    {songname:"Tum_Se_Hi", filepath:"Documents/GitDemo/Spotify/Tum_Se_Hi.mp3",coverpath:"Documents/GitDemo/Spotify/song10.jpeg"},
+    {songname:"Bahara", filepath:"Bahara1.mp3",coverpath:"song1.jpg"},
+    {songname:"Dil Tu Jaan Tu", filepath:"Dil_Tu_Jaan_Tu.mp3",coverpath:"song2.jpeg"},
+    {songname:"Excuses", filepath:"Excuses.mp3",coverpath:"song3.jpeg"},
+    {songname:"Heroine", filepath:"Heroine4.mp3",coverpath:"song4.jpeg"},
+    {songname:"Jaane Kyun", filepath:"Jaane Kyun.mp3",coverpath:"song5.jpeg"},
+    {songname:"Tu Mileya", filepath:"Tu_Mileya.mp3",coverpath:"song6.jpeg"},
+    {songname:"Raatan Lambiyan", filepath:"Raatan_Lambiyan.mp3",coverpath:"song7.jpeg"},
+    {songname:"Tum Jo Aaye", filepath:"Tum_Jo_Aaye.mp3",coverpath:"song8.jpeg"},
+    {songname:"Tum Se", filepath:"Tum_Se.mp3",coverpath:"song9.jpeg"},
+    {songname:"Tum_Se_Hi", filepath:"Tum_Se_Hi.mp3",coverpath:"song10.jpeg"},
 ]
 
 
  masterplay.addEventListener('click',()=>{
-    if(audi0.paused || audio.currentTime<=0){
+    if(audio.paused || audio.currentTime<=0){
         audio.play();
-        masterplay.classList.remove("fa-regular fa-circle-play");
-        masterplay.classList.add("fa-regular fa-circle-pause");
+        masterplay.classList.remove("fa-circle-play");
+        masterplay.classList.add("fa-circle-pause");
         gif.style.opacity = 1;
     }
     else{
         audio.pause();
-        masterplay.classList.remove("fa-regular fa-circle-pause");
-        masterplay.classList.add("fa-regular fa-circle-play");
+        masterplay.classList.remove("fa-circle-pause");
+        masterplay.classList.add("fa-circle-play");
         gif.style.opacity = 0;
     }
  })
@@ -46,3 +46,10 @@ audio.addEventListener("Timeupdate",()=>{
  progress_bar.addEventListener("change",()=>{
     audio.currentTime=((progress_bar.value * audio.duration)/100);
  })
+ let song_item=Array.from(document.getElementsByClassName('song_items'));
+song_item.forEach((element,i)=>{
+    console.log(element);
+   element.getElementsByClassName('song_img')[i].src=song[i].coverpath;
+   element.getElementsByClassName('songname')[i].innerText=song[i].songname;
+})
+
---
+++ script2.js
@@ -0,0 +1,23 @@
+// let array=[
+//     "Nancy","Shruti","Kritika"
+// ]
+// array.forEach((val)=>{
+//     console.log(val);
+//     console.log(val[0]);
+// })
+
+
+// let array2=[
+//     {name:"Nancy",friendname:"Shruti",year:"2nd"},
+//     {name:"Shruti",friendname:"Nancy",year:"3rd"},
+//     {name:"Kritika",friendname:"Nancy",year:"2nd"}
+// ];
+// array2.forEach((val)=>{
+//     console.log(val);
+// })
+
+
+// let song_items=Array.from(document.getElementsByClassName(".song_items"));
+// song_items.forEach((element,i)=>{
+//     console.log(element[i]);
+// })
